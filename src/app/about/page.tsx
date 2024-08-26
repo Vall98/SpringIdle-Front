55a5d12caeef58707e09b1473dd1e39687ff1025
Valentin <valentin.lebon@epitech.eu>
2024-08-26 22:43:50 +0100

    [ADD] navbar and about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const About = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p>
+          This is a small project made by Vall98 (
+          <Link href="https://github.com/Vall98"
+            target="_blank" rel="noopener noreferrer"
+            className="text-blue-600 hover:text-blue-800 font-semibold">
+            GitHub
+          </Link>
+          ).<br/>
+          The website is developed with Next.js (React) and the API with FastAPI (Python).<br/>
+          The project is deployed with GitHub Actions on Vercel and AWS.<br/>
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default About;
--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -6,28 +6,38 @@ const Navbar = () => {
   const authContext = useAuthContext();
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        {authContext.isAuthenticated ? (
-          <>
-            <li>
-              <button onClick={authContext.signout}>Sign Out</button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link href="/login">Sign In</Link>
-            </li>
-            <li>
-              <Link href="/join">Sign Up</Link>
-            </li>
-          </>
-        )}
-      </ul>
+    <nav className="bg-[#22313f] p-4 absolute w-screen">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-xl font-bold">
+          <Link href="/">SpringIdle</Link>
+        </div>
+        <ul className="md:flex space-x-6">
+          <li>
+            <Link href="/about" className="text-white hover:text-gray-300">
+              About
+            </Link>
+          </li>
+          {authContext.isAuthenticated ? (
+            <>
+              <li>
+                <Link href="/me" className="text-white hover:text-gray-300">Profile</Link>
+              </li>
+              <li>
+                <button onClick={authContext.signout} className="text-white hover:text-gray-300">Sign Out</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/login" className="text-white hover:text-gray-300">Sign In</Link>
+              </li>
+              <li>
+                <Link href="/join" className="text-white hover:text-gray-300">Sign Up</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
